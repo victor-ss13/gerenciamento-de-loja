31dbd6993d7e8c8da57feeee32472e4077f690d0
Victor Sampaio <victor.sampaio2003@gmail.com>
2025-11-20 02:46:22 -0300

    Develop (#1)
    * instalação dos pacotes do EF
    
    * feat: criando classes e setando Cliente, Categoria e Produto
    
    * feat: finalizando set das classes

---
+++ LojaGerenciamento.Domain/Classes/Pedido.cs
@@ -0,0 +1,43 @@
+﻿namespace LojaGerenciamento.Domain.Classes
+{
+    public class Pedido
+    {
+        public int IdPedido { get; private set; }
+        public DateTime Data { get; private set; } = DateTime.Now;
+        public int IdCliente { get; private set; }
+        public Cliente Cliente { get; private set; }
+        public string Situacao { get; private set; }
+        public ICollection<ItemPedido> Itens { get; private set; } = new List<ItemPedido>();
+
+        public Pedido() { }
+
+        public Pedido(Cliente cliente)
+        {
+            Cliente = cliente;
+            Situacao = "Ativo";
+
+            Validar();
+        }
+
+        private void Validar()
+        {
+            if (Cliente == null)
+                throw new Exception("Cliente é obrigatório");
+        }
+
+        public void Alterar(Cliente cliente)
+        {
+            Cliente = cliente;
+
+            Validar();
+        }
+
+        public void Excluir()
+        {
+            if (Situacao != "Ativo")
+                throw new Exception("Apenas pedidos ativos podem ser excluídos");
+
+            Situacao = "Excluido";
+        }
+    }
+}
