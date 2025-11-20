bab36083233610eef67ed3b093274be91d0541bb
victor-ss13 <victor.sampaio2003@gmail.com>
2025-11-20 02:45:52 -0300

    feat: finalizando set das classes

--- LojaGerenciamento.Domain/Classes/ItemPedido.cs
+++ LojaGerenciamento.Domain/Classes/ItemPedido.cs
@@ -2,5 +2,56 @@
 {
     public class ItemPedido
     {
+        public int IdItemPedido { get; private set; }
+        public int IdPedido { get; private set; }
+        public Pedido Pedido { get; private set; }
+        public int IdProduto { get; private set; }
+        public Produto Produto { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal Preco { get; private set; }
+        public string Situacao { get; private set; }
+
+        public ItemPedido() { }
+
+        public ItemPedido(Pedido pedido, Produto produto, int quantidade, decimal preco)
+        {
+            Pedido = pedido;
+            Produto = produto;
+            Quantidade = quantidade;
+            Preco = preco;
+            Situacao = "Ativo";
+
+            Validar();
+        }
+
+        private void Validar()
+        {
+            if (Pedido == null)
+                throw new Exception("Pedido é obrigatório");
+            if (Produto == null)
+                throw new Exception("Produto é obrigatório");
+            if (Quantidade <= 0)
+                throw new Exception("Valor inválido para quantidade");
+            if (Preco <= 0)
+                throw new Exception("Valor inválido para preço");
+        }
+
+        public void Alterar(Pedido pedido, Produto produto, int quantidade, decimal preco)
+        {
+            Pedido = pedido;
+            Produto = produto;
+            Quantidade = quantidade;
+            Preco = preco;
+
+            Validar();
+        }
+
+        public void Excluir()
+        {
+            if (Situacao != "Ativo")
+                throw new Exception("Apenas itens ativos podem ser excluídos");
+
+            Situacao = "Excluido";
+        }
     }
 }
--- LojaGerenciamento.Domain/Classes/Pagamento.cs
+++ LojaGerenciamento.Domain/Classes/Pagamento.cs
@@ -2,5 +2,57 @@
 {
     public class Pagamento
     {
+        public int IdPagamento { get; private set; }
+        public int IdPedido { get; private set; }
+        public Pedido Pedido { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataPagamento { get; private set; } = DateTime.Now;
+        public string Metodo { get; private set; }
+        public string Situacao { get; private set; }
+
+        public Pagamento() { }
+
+        public Pagamento(Pedido pedido, decimal valor, string metodo)
+        {
+            Pedido = pedido;
+            Valor = valor;
+            Metodo = metodo;
+            Situacao = "Ativo";
+
+            Validar();
+        }
+
+        private void Validar()
+        {
+            if (Pedido == null)
+                throw new Exception("Pedido é obrigátório");
+            if (Valor <= 0)
+                throw new Exception("Valor inválido");
+            if (string.IsNullOrWhiteSpace(Metodo))
+                throw new Exception("Método de pagamento é obrigatório");
+        }
+
+        // Não imagino que um pagamento deva ser alterado, porém vou manter o método de alterar caso seja necessário futuramente
+        public void Alterar(Pedido pedido, decimal valor, string metodo)
+        {
+            Pedido = pedido;
+            Valor = valor;
+            Metodo = metodo;
+
+            Validar();
+        }
+
+        // Não imagino que pagamentos possam ou devam ser excluídos, mas...
+        public void Excluir()
+        {
+            if (Situacao != "Ativo")
+                throw new Exception("Apenas pagamentos ativos podem ser excluídos");
+
+            // Restringir exclusão de pagamentos apenas para pagamentos futuros (passados permanecem estáticos)
+            if (DataPagamento.Date < DateTime.Now)
+                throw new Exception("Não é possível excluir pagamento de data anterior");
+
+            Situacao = "Excluido";
+        }
     }
 }
